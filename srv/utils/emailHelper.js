994ee8152a7b51c6cc13fb2635dfd360e5bff247
Gashaw <gtforever2009@gmail.com>
2023-12-22 09:10:36 +0300

    commit

--- srv/utils/emailHelper.js
+++ srv/utils/emailHelper.js
@@ -1,36 +1,36 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-async function sendEmail(){
+// async function sendEmail(){
     
-    const to = "molabsc@gmail.com"
-    const subject = "just another test"
-    const text = "another test content"
+//     const to = "molabsc@gmail.com"
+//     const subject = "just another test"
+//     const text = "another test content"
 
-    const transporter = nodemailer.createTransport({
-      host: 'mail.bernos.info',
-      port: 465,
-      secure: true,
-      auth: {
-        user: 'muluken@bernos.info',
-        pass: '!baKyY7ue%e8',
-      },
-    });
+//     const transporter = nodemailer.createTransport({
+//       host: 'mail.bernos.info',
+//       port: 465,
+//       secure: true,
+//       auth: {
+//         user: 'muluken@bernos.info',
+//         pass: '!baKyY7ue%e8',
+//       },
+//     });
   
   
-    const mailOptions = {
-      from: 'muluken@bernos.info',
-      to,
-      subject,
-      text,
-    };
+//     const mailOptions = {
+//       from: 'muluken@bernos.info',
+//       to,
+//       subject,
+//       text,
+//     };
   
-    try {
-      // Send the email
-      const info = await transporter.sendMail(mailOptions);
-      console.log('Email sent: ', info);
+//     try {
+//       // Send the email
+//       const info = await transporter.sendMail(mailOptions);
+//       console.log('Email sent: ', info);
   
-    } catch (error) {
-      console.error('Error sending email: ', error);
-    }
-}
-module.exports = { sendEmail };
+//     } catch (error) {
+//       console.error('Error sending email: ', error);
+//     }
+// }
+// module.exports = { sendEmail };
